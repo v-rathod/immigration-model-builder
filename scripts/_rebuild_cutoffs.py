5e13c3e4d6eeddbdf38c3d0341fecfc7f3e6e7c7
Rathod, Vivek S <Vivek.Rathod@optum.com>
2026-03-04 15:11:52 -0600

    chore: delete 83 temporary investigation scripts

--- scripts/_rebuild_cutoffs.py
+++
@@ -1,9 +0,0 @@
-"""Rebuild fact_cutoffs from scratch with the fixed parser."""
-import yaml
-from src.curate.visa_bulletin_loader import load_visa_bulletin
-
-with open('configs/paths.yaml') as f:
-    cfg = yaml.safe_load(f)
-
-result = load_visa_bulletin(cfg['data_root'], cfg['artifacts_root'])
-print(f'Output: {result}')
